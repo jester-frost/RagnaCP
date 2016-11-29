c90ec3bd9c45b8ae83f64f2faa72680dc1f8e375
Marcos <marcos@visie.com.br>
2016-11-29 13:49:19 -0200

    Bug de resumo duplicado resolvido

--- RagnaCP/conteudo_publico.php
+++ RagnaCP/conteudo_publico.php
@@ -2,7 +2,9 @@
 /* Template Name: [ Conteudo Publico ] */
 include_once 'includes/config.php'; // loads config variables
 include_once 'includes/functions.php';
+
 $resumo = get_the_excerpt();
+
 get_header();
 ?>
 
@@ -30,7 +32,7 @@ get_header();
 
                     <?php }; ?>
 
-                    <p><?php the_excerpt(); ?></p>
+                    <p><?php the_content(); ?></p>
 
 
                 </div>
