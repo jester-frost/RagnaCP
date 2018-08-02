299f861f79e5a7047e0fab9dd0b957dc03bf731a
Marcos Gonçalves <marcos@visie.com.br>
2018-08-02 17:58:10 -0300

    Verificação de variaveis

--- RagnaCP/includes/doacao_pendente.php
+++ RagnaCP/includes/doacao_pendente.php
@@ -22,39 +22,43 @@
         </tr>
     </thead>
     <tbody>
-    <?php 
-        include 'analise.php';
-        $i = 0;
-        foreach ($doacoes as $c) :
-            $i = $i+1;
-    ?>
-        <tr>
-            <td>
-                <div align="center">
-                    <?php echo $i;?>
-                </div>
-            </td>
-            <td>
-                <div align="center">
-                    <?php echo $c->data;?>
-                </div>
-            </td>
-            <td>
-                <div align="center">
-                    <?php echo "R$ ".$c->valor.",00";?>
-                </div>
-            </td>
-            <td>
-                <div align="center">
-                    <?php echo $c->Rops;?>
-                </div>
-            </td>
-            <td>
-                <div align="center">
-                    <?php echo $sts[$c->estado]; ?>
-                </div>
-            </td>
-        </tr>
-    <?php endforeach; ?>
+        <?php if( $doacoes ): ?>
+        <?php 
+            include 'analise.php';
+            $i = 0;
+            foreach ($doacoes as $c) :
+                $i = $i+1;
+        ?>
+            <tr>
+                <td>
+                    <div align="center">
+                        <?php echo $i;?>
+                    </div>
+                </td>
+                <td>
+                    <div align="center">
+                        <?php echo $c->data;?>
+                    </div>
+                </td>
+                <td>
+                    <div align="center">
+                        <?php echo "R$ ".$c->valor.",00";?>
+                    </div>
+                </td>
+                <td>
+                    <div align="center">
+                        <?php echo $c->Rops;?>
+                    </div>
+                </td>
+                <td>
+                    <div align="center">
+                        <?php echo $sts[$c->estado]; ?>
+                    </div>
+                </td>
+            </tr>
+        <?php endforeach; ?>
+        <?php else: ?>
+            <p>Sem Doações pendentes.</p>
+    <?php endif; ?>
     </tbody>
 </table>
